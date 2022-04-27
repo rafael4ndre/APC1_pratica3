5aeff707791dba614055f72b590cc122a0abb094
Rafael Oliveira <rafael.andre.oliveira@gmail.com>
2022-04-27 20:03:10 -0300

    "test commit"

--- questao01.c
+++ questao01.c
@@ -5,7 +5,8 @@ Faça um programa em C que determine se um número inteiro lido é par ou ímpar
 #include <stdio.h>
 
 int main () {
-    printf("Hello World")
+    printf("Hello World"); 
+    printf("Hello World2")
 
     return 0; 
 }
